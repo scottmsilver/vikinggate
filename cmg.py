6d067f4b0e9bda81c5cd9136b826a79da3fa67ea
Scott M. Silver <scott.m.silver@alum.dartmouth.org>
2019-12-16 01:45:11 +0000

    first try at controlling the raspberry pi with relay board

---
+++ cmg.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+from enum import Enum
+    
+while False:
+    time.sleep(1)
+    print("high")
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(1)
+    print("low")
+    GPIO.output(pin, GPIO.LOW)
+
+class GateController:
+    class Relay(Enum):
+        ONE = 1, 37    # P25 = GPIO26, Pin 37
+        TWO = 2, 38    # P28 = GPIO20, Pin 38
+        THREE = 3, 40  # P29 = GPIO21, Pin 40
+
+        def __new__(clazz, relay, pin):
+            obj = object.__new__(clazz)
+            obj._value_ = relay
+            obj.pin = pin
+            return obj
+    
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        for relay in GateController.Relay:
+            GPIO.setup(relay.pin, GPIO.OUT)
+
+    def setRelay(self, relay, high):
+        GPIO.output(relay.pin, GPIO.HIGH if high else GPIO.LOW)
+    
+controller = GateController()
+
+state = True
+while True:
+    time.sleep(1)
+    controller.setRelay(GateController.Relay.ONE, state)
+    time.sleep(1)
+    controller.setRelay(GateController.Relay.TWO, state)
+    time.sleep(1)
+    controller.setRelay(GateController.Relay.THREE, state)    
+    state = not state
